13dd35db9c61f99336663878f480bf4fba47ee97
stokercg2913 <stokercg2913@gmail.com>
2017-10-08 15:44:12 -0500

    Added Submit form for _Get array redirect.

--- test31.php
+++ test31.php
@@ -1,1 +1,8 @@
-<div>test</div>
+<form action="test21.php" method="get">
+    <label>First Name:</label>
+    <input type="text" name="first_name"/><br />
+    <label>Last Name:</label>
+    <input type="text" name="last_name"/><br />
+    <label>First Name:</label>
+    <input type="submit" value="Submit"/><br />
+</form>
