ddfac263c5e14351ad0b1b4f3da8f3e3d061d427
stokercg2913 <stokercg2913@gmail.com>
2017-10-09 11:12:56 -0500

    Added input filter for global post array on display page

--- test21.php
+++ test21.php
@@ -1,7 +1,7 @@
 <?php
-$firstname = $_POST['first_name'];
-$lastname = $_POST['last_name'];
-$dollars = $_POST['dollars'];
+$firstname = filter_input(INPUT_POST, 'first_name');
+$lastname = filter_input(INPUT_POST, 'last_name');
+$dollars = filter_input(INPUT_POST, 'dollars', FILTER_VALIDATE_FLOAT);
 ?>
 
 <!doctype html>
@@ -17,7 +17,7 @@ $dollars = $_POST['dollars'];
 <body>
 <p><?php echo 'First Name: ' . htmlspecialchars($firstname); ?></p>
 <p><?php echo 'Last Name: ' . htmlspecialchars($lastname); ?></p>
-<p><?php echo 'Dollars in Bank: ' . htmlspecialchars(number_format($dollars, 2)); ?></p>
+<p><?php echo 'Dollars in Bank: $' . htmlspecialchars(number_format($dollars, 2)); ?></p>
 <p><?php echo 'Date: ' . htmlspecialchars(date('m/d/y')); ?></p>
 </body>
 </html>
